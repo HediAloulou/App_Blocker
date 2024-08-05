cc7af6be819e78fe3f2b44df4835e0aec94fb2c2
Hedi ALOULOU <haloulou@sofrecom.fr>
2024-08-05 10:58:19 +0100

    first

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
+}
+
+android {
+    namespace = "com.example.prediction"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.prediction"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        mlModelBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.firebase.firestore)
+    implementation(libs.tensorflow.lite.support)
+    implementation(libs.tensorflow.lite.metadata)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+<<<<<<< HEAD
+    implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
+=======
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+>>>>>>> origin/master
+    implementation("com.google.firebase:firebase-firestore")
+
+
+}
